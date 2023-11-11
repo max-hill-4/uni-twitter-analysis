51a03ec2a5321a866ffd3f915ad3c9e6c001e187
Max <maxwellhill2004@icloud.com>
2023-11-11 21:15:30 +0000

    Delete credentials.py

--- backend/tweet-analysis/credentials.py
+++
@@ -1,8 +0,0 @@
-# Replace these with your own Twitter API credentials
-consumer_key = 'your_consumer_key'
-consumer_secret = 'your_consumer_secret'
-access_token = 'your_access_token'
-access_token_secret = 'your_access_token_secret'
-
-
-# THIS IS A CHANGE 
