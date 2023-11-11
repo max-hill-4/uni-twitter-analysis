af2de3b18ec9d03ca10e0643818f80ded480dc9c
Max <maxwellhill2004@icloud.com>
2023-11-11 16:50:30 +0000

    Added GET method

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__, template_folder='../frontend/templates')
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ frontend/src/index.js
@@ -0,0 +1,3 @@
+    document.getElementById("searchButton").addEventListener("click", function() {
+        console.log("Button clicked!");
+    });
