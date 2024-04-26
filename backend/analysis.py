c0ca9c4a1599537189301e12223be5a0e0032614
Max Hill <43377490+max-hill-4@users.noreply.github.com>
2024-04-26 14:08:05 +0100

    added NLTK's Vader

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,7 +1,16 @@
 import get_tweets
-import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import twitter_samples
+
+# These are needed! - dont want to run checks everytime
+#from nltk import download
+#download('vader_lexicon')
+#download('twitter_samples')
 
 # Not happy with double parsing of query
-def analyze_tweet(query):
-    data = get_tweets.get_tweet(query)
-    return data
+
+async def analyze_tweet(query):
+    data = await get_tweets.get_tweet(query)
+    sia = SentimentIntensityAnalyzer()
+    score = sia.polarity_scores(data)["compound"]
+    return {data : score}
