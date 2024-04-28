04b14d0030ecdcf13892314d7b105b3a10ad8d87
Max Hill <43377490+max-hill-4@users.noreply.github.com>
2024-04-28 15:55:40 +0100

    change

--- backend/get_tweets.py
+++ backend/get_tweets.py
@@ -11,7 +11,8 @@ async def get_tweet(tweet):
     r = r.json()['html']
 
     soup = BeautifulSoup(r, 'html.parser')
-    return (soup.find('blockquote').find('p').get_text(strip=True))
+    text = """<blockquote class="twitter-tweet"><p lang="en" dir="ltr">When I was writing the Fortnight music video, I wanted to show you the worlds I saw in my head that served as the backdrop for making this music.  Pretty much everything in it is a metaphor or a reference to one corner of the album or another. For me, this video turned out to be… <a href="https://t.co/TLaUg9jEoo">pic.twitter.com/TLaUg9jEoo</a></p>&mdash; Taylor Swift (@taylorswift13) <a href="https://twitter.com/taylorswift13/status/1781474914542756273?ref_src=twsrc%5Etfw">April 20, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> """
+    return (text)
 
 # We would probably maybe have two gets, where one is ust for the embed code, 
 # and the other for sentiment data! we would pass both ytohugh to jinja!
