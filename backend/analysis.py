011fb9f0167d6656e76ed72f03c0c33113eac11a
Max Hill <43377490+max-hill-4@users.noreply.github.com>
2024-04-30 17:29:17 +0100

    finished bow
    it honestly was not worth it hahaha

--- backend/analysis.py
+++ backend/analysis.py
@@ -8,6 +8,7 @@ from nltk import TweetTokenizer
 from nltk import download
 from nltk import NaiveBayesClassifier
 from nltk import classify
+from nltk import FreqDist
 
 from nltk.stem import WordNetLemmatizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -17,16 +18,15 @@ from nltk.corpus import twitter_samples
 from statistics import mean
 
 corpora = ['vader_lexicon', 'twitter_samples', 'stopwords', 'wordnet', 'averaged_perceptron_tagger']
-# Need to find way to check if already installed
+# (low) check if corpara exists.
 for i in corpora:
      download(i)
 
 """
--> Feature Selection
-    Tag the data with our 'feature data' - (Frequency, VADER score , etc.)
-    Tag the data with the positive or negative 'actual' data
-
 TD? 
+
+-> use pandas for data - its getting confusing!
+
 -> are we going to train a entire model using a classifier everytime I load the program? 
 -> could i create train model and use an api to call to it? 
 
@@ -34,25 +34,24 @@ TD?
 
 -> return important features (highlight?)
 
+-> plot visual data DL
 
 
-
+References: 
+real python
 https://necromuralist.github.io/Neurotic-Networking/posts/nlp/01-twitter-preprocessing-with-nltk/index.html
 """
-# we could make a cool plot to prove under fitting and over fitting
 class ProcessData:
-    # make functions priva using _
-    def __init__(self, text: list,  label: str = None):
-        # Text is passed as unclean data.
-        self.label = label
+    def __init__(self, text: list):
         self.text = text
-        self.lemmatizer = WordNetLemmatizer()
         self.STOPWORDS = set(stopwords.words("english"))
         
         self.tokenizer = TweetTokenizer(preserve_case=False,
                                         strip_handles=True,
                                         reduce_len=True)
         
+        self.lemmatizer = WordNetLemmatizer()
+
         self.TAGMAP = {'V' : 'v', 'J' : 'a', 'N' : 'n', 'R' : 'r' }
 
 
@@ -64,79 +63,85 @@ class ProcessData:
     def process_text(self) -> list[list[str]]:
         
         tokens = []
-        #should convert sentence -> tweet
-        for sentence in self.text:
-            data = []
-            # tokenize the sentence
-            data = self.tokenizer.tokenize(sentence)
 
-            # remove stopwords
+        for tweet in self.text:
+            
+            data = self.tokenizer.tokenize(tweet)
+
             data = [token for token in data if token.isalpha() and token not in self.STOPWORDS]
 
-            # if data is empty, dont bother with rest of the loop.
-
-            # Pull request needed for pos tag -> lemmas tag!
+            # (low) TD: Pull request pos tag (tagset) to work with lemmatize
             data = pos_tag(data)
         
-            data_copy = []
-            for token, pos in data:
-                data_copy.append(self.lemmatizer.lemmatize(token, self.map_pos(pos)))
-                                 
+            # (low) TD: refact if data + lemmatize + pos_tag
             
             data = [self.lemmatizer.lemmatize(token, self.map_pos(pos)) for token, pos in data]
             
-            # un categorised data using a 'None' tag. should be changed      
             if data:
-                tokens.append((data, self.label))
+                tokens.append(data)
         
         return tokens
     
 class TrainModel:
 
-    def __init__(self):
-        self.sia = SentimentIntensityAnalyzer()
-        self.unigram = set()
-    
-    def _calc_feature(self, tweet: list[str, str]) -> dict:
+    def __init__(self, pos_text, neg_text):
         
+        self.sia = SentimentIntensityAnalyzer()
+        self._pos_text = pos_text
+        self._neg_text = neg_text
+
+        self._freq = FreqDist([item for sublist in pos_text for item in sublist])
+        
+        self._bow = [word for word, _ in self._freq.most_common(10)]
+    
+    def _calc_tweet(self, tweet) -> dict:
         features = {}
         compound_scores = []
         positive_scores = []
-
-        for word in tweet[0]:
+        bow = 0
+        for word in tweet:
             compound_scores.append(self.sia.polarity_scores(word)["compound"])
             positive_scores.append(self.sia.polarity_scores(word)["pos"])
-
+            if word in self._bow:  
+                bow += 1
+        
         features['mean_compound'] = mean(compound_scores) 
         features['mean_positive'] = mean(positive_scores)
+        # it actually became less accurate with this data lol!
+        features['bow'] = bow
 
         return features
 
 
-    def train(self, text):
-       
+    def train(self):
+     
+        # [({'value' : 1}, 'p'), ]
         features = []
-       
-        for tweet in text:
-            features.append((self._calc_feature(tweet), tweet[1]))
-       
-        shuffle(features)
+        for tweet in self._pos_text:
+            features.append((self._calc_tweet(tweet), 'p'))
         
-        classifier = NaiveBayesClassifier.train(labeled_featuresets=features)
+        for tweet in self._neg_text:
+            features.append((self._calc_tweet(tweet), 'n'))
+         
+        shuffle(features)
+        classifier = NaiveBayesClassifier.train(labeled_featuresets=features[:1500])
         classifier.show_most_informative_features(5)
+        print(classify.accuracy(classifier, features[1500:]))
         return classifier
     
+# should have a function in proccess data so i can just read(*.txt)
 
 pos_tweet = twitter_samples.strings('positive_tweets.json')
 neg_tweet = twitter_samples.strings('negative_tweets.json')
 
-pos_data = ProcessData(pos_tweet, 'p').process_text()
-neg_data = ProcessData(neg_tweet, 'n').process_text()
-
-model = TrainModel().train(pos_data + neg_data)
+pos_data = ProcessData(pos_tweet).process_text()
+neg_data = ProcessData(neg_tweet).process_text()
 
 
+model = TrainModel(pos_data, neg_data).train()
 
+
+# im going to seperate the texts, it helps with empty labels + freqdist.
 
 
 
