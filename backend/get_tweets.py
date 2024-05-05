13e25798b61962258f999424bdaf3bc025bce8eb
Max Hill <43377490+max-hill-4@users.noreply.github.com>
2024-05-05 15:51:05 +0100

    Update get_tweets.py

--- backend/get_tweets.py
+++ backend/get_tweets.py
@@ -1,15 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
 
 async def get_tweet(tweet):
-    """
-    This is a request using Twitter's 
-    """
-    tweet = "https://twitter.com/taylorswift13/status/1781474914542756273?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Etweet"
-
     r = requests.get("https://publish.twitter.com/oembed?url=%s" % tweet)
     r = r.json()['html']
-
-    soup = BeautifulSoup(r, 'html.parser')
-    print(r)
     return (r)
