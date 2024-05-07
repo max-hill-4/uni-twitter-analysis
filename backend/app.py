9364b13e46db4d108fc85c3be6dc2074baa6abf7
Max Hill <43377490+max-hill-4@users.noreply.github.com>
2024-05-07 14:39:05 +0100

    added comments for docs

--- backend/app.py
+++ backend/app.py
@@ -6,11 +6,16 @@ from ML import NaiveBayes, NeuralNetwork
 
 @app.route('/')
 def index():
-    """This is for the main route in the flask file"""
+    """
+    Flask route to return the main search page.
+    """
     return render_template('index.html')
 
 @app.route('/search')
 def search():
+    """
+    Search page that visualizes the sentiment data of the tweet.
+    """
     q = request.args.get('query')
     loop = new_event_loop()
     tweet = loop.run_until_complete(get_tweet(q))
@@ -18,6 +23,13 @@ def search():
 
 @app.route('/naivebayes', methods=['GET'])
 def naivebayes():
+    """
+    Uses the ML NaiveBayes Module to perform sentiment analysis.
+    Args:
+        query (str) : The url of the tweet
+    Returns:
+        JSON: 'p' or 'n' value dictating sentiment.
+    """
     data = request.args.get('query')
     loop = new_event_loop()
     data = loop.run_until_complete(NaiveBayes.NaiveBayes().predict(data))
@@ -25,6 +37,13 @@ def naivebayes():
 
 @app.route('/neuralnetwork', methods=['GET'])
 def neuralnetwork():
+    """
+    Uses the ML Neural Netowrk Module to perform sentiment analysis.
+    Args:
+        query (str) : The url of the tweet
+    Returns:
+        JSON: 'p' or 'n' value dictating sentiment.
+    """
     data = request.args.get('query')
     loop = new_event_loop()
     data = loop.run_until_complete(NeuralNetwork.NeuralNetwork().predict(data))
--- backend/get_tweets.py
+++ backend/get_tweets.py
@@ -1,6 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
 async def get_tweet(tweet:str):
+    """
+    Uses Twitter's Oemebed to return tweet data.
+    Args:
+        query (str) : The url of the tweet
+    Returns:
+        JSON: html data of the embeded tweet.
+    """
     r = requests.get("https://publish.twitter.com/oembed?url=%s" % tweet)
     r = r.json()['html']
     return r 
