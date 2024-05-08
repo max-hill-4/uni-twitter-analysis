464ddb23659d89b485da5c1b90a65de6e56b5947
Jmurdock2003 <jacksonmurdock11@gmail.com>
2024-05-08 02:12:39 +0100

    Updated UI
    Currently getting this error "AttributeError: 'TwitterCorpusReader' object has no attribute '_LazyCorpusLoader__args' . The UI has been updated but the naive bayes model should be called before neural network. The error needs fixing too

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -12,12 +12,17 @@ async function fetchML(endpoint, place) {
 
 function updateStatus(status, place) {
     const statusBox = document.getElementById(place);
+    const sentimentText = status === 'p' ? 'Positive' : 'Negative';
+    const sentimentSpan = document.getElementById(place + '-sentiment');
+    sentimentSpan.textContent = sentimentText;
+
     if (status === 'p') {
-        statusBox.style.backgroundColor = 'green';
+        statusBox.style.backgroundColor = "#42f545";
     } else {
-        statusBox.style.backgroundColor = 'red';
+        statusBox.style.backgroundColor = "#f54242";
     }
-  }
+}
+
 const queryString = document.getElementById('tweet').textContent;
 
 fetchML('/neuralnetwork', 'nn');
