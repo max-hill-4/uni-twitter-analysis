a4091698ce4374098f0d692690193c26ec77e92b
Max Hill <43377490+max-hill-4@users.noreply.github.com>
2024-05-08 20:07:06 +0100

    yot

--- backend/ML/testing.py
+++ backend/ML/testing.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import patch
 import joblib
 from NaiveBayes import NaiveBayes
-
+from NeuralNetwork import NeuralNetwork
 
 class TestNaiveBayes(unittest.TestCase):
     
@@ -12,13 +12,24 @@ class TestNaiveBayes(unittest.TestCase):
 
         tweet = "this is a tweet with stopwords"
         _STOPWORDS = ["a", "the", "is", "with"]
-        data = mock_method(tweet)
-        expected_data = []
-        for word in tweet.split():
-            if word not in _STOPWORDS:
-                expected_data.append(word)
-        self.assertEqual()
+        data = NaiveBayes()._preprocess(tweet)
+        expected_data = expected_data = ['this', 'tweet', 'stopwords']
+        self.assertEqual(data, expected_data)
 
+class TestNeuralNetwork(unittest.TestCase):
+    def setUp(self):
+        # Initialize your NeuralNetwork instance with appropriate data
+        self.neural_network = NeuralNetwork()
+
+    def test_preprocess_sequence_length(self):
+        # Mock input data
+        tweets = ["This is a test tweet."]
+
+        # Call the _preprocess method
+        processed_sequences = self.neural_network._preprocess(tweets)
+
+        # Check if the length of the processed sequence is 100
+        self.assertEqual(len(processed_sequences[0]), 100, "Length of processed sequence is not 100")
 
 if __name__ == "__main__":
     unittest.main()
